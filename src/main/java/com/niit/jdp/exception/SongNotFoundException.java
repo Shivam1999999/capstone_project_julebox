8da17d4e9abe4380e81baa183065dc723c186764
shivammanitripathi007 <shivammanitripathi007@gmail.com>
2022-09-26 11:30:23 +0530

    docs are generated for SongNotFoundException

--- src/main/java/com/niit/jdp/exception/SongNotFoundException.java
+++ src/main/java/com/niit/jdp/exception/SongNotFoundException.java
@@ -1,5 +1,9 @@
 package com.niit.jdp.exception;
 
+/**
+ * SongNotFoundException is a class that extends Throwable and has a constructor that takes a String and a getter for the
+ * exceptionMessage.
+ */
 public class SongNotFoundException extends Throwable {
 
     private String exceptionMessage;
