fdadb649c3e2353e4e7f31faf00d188bc5e8fa0f
shivammanitripathi007 <shivammanitripathi007@gmail.com>
2022-09-26 11:31:07 +0530

    docs are generated for PlaylistNotFoundException

--- src/main/java/com/niit/jdp/exception/PlaylistNotFoundException.java
+++ src/main/java/com/niit/jdp/exception/PlaylistNotFoundException.java
@@ -1,6 +1,10 @@
 package com.niit.jdp.exception;
 
 
+/**
+ * This class is a custom exception class that extends the Exception class. It has a constructor that takes in a String and
+ * sets the exceptionMessage to a custom message. It also has a getter method for the exceptionMessage
+ */
 public class PlaylistNotFoundException extends Exception {
     private String exceptionMessage;
 
